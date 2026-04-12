ea283901aa8e75335129f1b055ff4bb6a4212438
jg8685-cmyk <jg8685@princeton.edu>
2026-04-12 12:21:23 -0400

    script renaming for refined repo logic

---
+++ scripts/perception.py
@@ -0,0 +1,18 @@
+# as-built mesh construction from depth image and brick pose detection by comparing meshes/depth images.
+
+# function that takes two images and retrieve precise brick pose, and checks reachability.
+
+# from depth point cloud images --> line up both frames and find the point cloud of the new block
+# then fit the block to the point cloud (icp) --> say that one side will always be flat
+# then extract pose and position from that block
+
+
+# iterate to see if the block is reachable
+
+# use joint constaints
+
+# returns this all to the sequencer
+
+# remembering function -- cases for recognizing it if the original structure is rotated + translated
+
+# use depth image to see what is there --> turn it into a mesh

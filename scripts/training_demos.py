b0d5b96c4531e8707883ec02a4d9b1d8178c52b4
Patti <145397567+patti-grate@users.noreply.github.com>
2026-04-14 19:59:52 -0400

    0414_changes

--- scripts/training_demos.py
+++ scripts/training_demos.py
@@ -4,3 +4,9 @@
 #the sequence will be computed by the sequencer and if the pose is reachable, 
 # it will be stored
 
+
+# using the realsense_params, collect images from camera and store it in the correct file structure and then take out the new poses for it - pg
+# store both the png and the npy array for depth 
+# 
+#  
+
